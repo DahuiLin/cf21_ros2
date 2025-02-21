1deb8fbb181c785cad410e31cf17cc68c8f912eb
DahuiLin <dahuilinyang@uma.es>
2025-02-21 14:34:18 +0100

    feat: add submodules packages

--- docker/docker_launch.bash
+++ docker/docker_launch.bash
@@ -12,6 +12,8 @@ docker run --privileged -it --rm \
     --volume=$XSOCK:$XSOCK:rw \
     --volume=$XAUTH:$XAUTH:rw \
     --volume /dev:/dev:rw \
+    --volume $(pwd)/../mocap_kalman_filter:/home/isa/catkin_ws/src/mocap_kalman_filter:rw \
+    --volume $(pwd)/../cf21_bridge_ros2:/home/isa/catkin_ws/src/cf21_bridge_ros2:rw \
     --env="XAUTHORITY=${XAUTH}" \
     --env=TERM=xterm-256color \
     ros_humble:latest \
