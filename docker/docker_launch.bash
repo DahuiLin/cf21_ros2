07bd6906f4c81a1bbfe4d4b4fc6666cebd1ff7bb
DahuiLin <dahuilinyang@uma.es>
2025-04-10 18:12:04 +0200

    fix: update repositories

--- docker/docker_launch.bash
+++ docker/docker_launch.bash
@@ -14,8 +14,6 @@ docker run --privileged -it --rm \
     --volume /dev:/dev:rw \
     --volume $(pwd)/../mocap_kalman_filter:/home/isa/catkin_ws/src/mocap_kalman_filter:rw \
     --volume $(pwd)/../cf21_bridge_ros2:/home/isa/catkin_ws/src/cf21_bridge_ros2:rw \
-    --volume /home/dahuipc/Desktop/My_path_trackers:/home/isa/catkin_ws/src/My_path_trackers:rw \
-    --volume /home/dahuipc/Desktop/improved_LCCMWTP:/home/isa/improved_LCCMWTP:rw \
     --volume $(pwd)/../uav_bridge:/home/isa/catkin_ws/src/uav_bridge:rw \
     --env="XAUTHORITY=${XAUTH}" \
     --env=TERM=xterm-256color \
